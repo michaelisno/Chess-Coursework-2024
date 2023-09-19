c19cdf83ea025cde35034b7a2e6b69e5655bc14c
Daniel <danielsampford1@gmail.com>
2023-09-19 16:06:45 +0100

    Began Menu

---
+++ Chess Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    { 
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+}
