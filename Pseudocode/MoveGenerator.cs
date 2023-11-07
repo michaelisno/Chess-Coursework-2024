8e1a08ed19372278b3910dc6a2f50e6d71da630e
Daniel <danielsampford1@gmail.com>
2023-11-07 08:30:23 +0000

    finished movegenerator pseudocode

--- Pseudocode/MoveGenerator.cs
+++ Pseudocode/MoveGenerator.cs
@@ -88,6 +88,199 @@ class MoveGenerator
                 if CheckLegality(tileIndex, PieceType.knight, isPlayerWhite) then
                     legalMoves.Append(GameManager.tiles[tileIndex])
                 endif
+
+                tileIndex = int(8 * position.x + position.y - 10)
+                if CheckLegality(tileIndex, PieceType.knight, isPlayerWhite) then
+                    legalMoves.Append(GameManager.tiles[tileIndex])
+                endif
+
+                tileIndex = int(8 * position.x + position.y + 15)
+                if CheckLegality(tileIndex, PieceType.knight, isPlayerWhite) then
+                    legalMoves.Append(GameManager.tiles[tileIndex])
+                endif
+
+                tileIndex = int(8 * position.x + position.y - 17)
+                if CheckLegality(tileIndex, PieceType.knight, isPlayerWhite) then
+                    legalMoves.Append(GameManager.tiles[tileIndex])
+                endif
+
+                if position.y == 0 then
+                    tileIndex = int(8 * position.x + position.y - 15)
+                    if CheckLegality(tileIndex, PieceType.knight, isPlayerWhite) then
+                        legalMoves.Append(GameManager.tiles[tileIndex])
+                    endif
+
+                    tileIndex = int(8 * position.x + position.y + 17)
+                    if CheckLegality(tileIndex, PieceType.knight, isPlayerWhite) then
+                        legalMoves.Append(GameManager.tiles[tileIndex])
+                    endif
+                endif
             endif
+        endif
+
+        # rook and lateral queen
+        if pieceType == PieceType.rook or pieceType == PieceType.queen then
+            for i = position.y to 7
+                tempIndex = int(8 * position.x + tileIndex + 1)
+                if CheckLegality(tempIndex, PieceType.rook, isPlayerWhite) then
+                    legalMoves.Add(GameManager.tiles[tempIndex])
+
+                    if GameManager.tiles[tempIndex].GetChild().Piece.Type != PieceType.none && GameManager.tiles[tempIndex].GetChild().Piece.GetColour() 
+                        != int(isPlayerWhite) then
+                        break
+                    endif
+                else
+                    break
+                endif
+            next i
+
+            for i = position.y to 0
+                tempIndex = int(8 * position.x + tileIndex - 1)
+                if CheckLegality(tempIndex, PieceType.rook, isPlayerWhite) then
+                    legalMoves.Add(GameManager.tiles[tempIndex])
+
+                    if GameManager.tiles[tempIndex].GetChild().Piece.Type != PieceType.none && GameManager.tiles[tempIndex].GetChild().Piece.GetColour() 
+                            != int(isPlayerWhite) then
+                        break
+                    endif
+                else
+                    break
+                endif
+            previous i
+
+            for i = position.y to 7
+                tempIndex = int(position.y + (tileIndex + 1)*8)
+                if CheckLegality(tempIndex, PieceType.rook, isPlayerWhite) then
+                    legalMoves.Add(GameManager.tiles[tempIndex])
+
+                    if GameManager.tiles[tempIndex].GetChild().Piece.Type != PieceType.none && GameManager.tiles[tempIndex].GetChild().Piece.GetColour() 
+                            != int(isPlayerWhite) then
+                        break
+                    endif
+                else
+                    break
+                endif
+            next i
+
+            for i = position.y to 0
+                tempIndex = int(position.y + (tileIndex - 1)*8)
+                if CheckLegality(tempIndex, PieceType.rook, isPlayerWhite) then
+                    legalMoves.Add(GameManager.tiles[tempIndex])
+
+                    if GameManager.tiles[tempIndex].GetChild().Piece.Type != PieceType.none && GameManager.tiles[tempIndex].GetChild().Piece.GetColour() 
+                            != int(isPlayerWhite) then
+                        break
+                    endif
+                else
+                    break
+                endif
+            next i
+        endif
+
+        # bishop and diagonal queen
+        if pieceType = PieceType.bishop or pieceType == PieceType.queen then
+            originPosition = int(8*position.x + position.y)
+            
+            for i = int(position.x * 8 + position.y + 9) to 63
+                if CheckLegality(isPlayerWhite, PieceType.bishop, isPlayerWhite, originPosition) then
+                    legalMoves.Add(GameManager.tiles[i])
+                
+                    if GameManager.tiles[i].GetChild().Piece.GetType() != PieceType.none && GameManager.tiles[testedPosition].GetChild().Piece.GetColour() 
+                            != int(isPlayerWhite) then
+                        break
+                    endif
+                else
+                    break
+                endif
+            next9th i
+
+            for i = int(position.x * 8 + position.y - 7) to 0
+                if CheckLegality(isPlayerWhite, PieceType.bishop, isPlayerWhite, originPosition) then
+                    legalMoves.Add(GameManager.tiles[i])
+
+
+                    if GameManager.tiles[i].GetChild().Piece.GetType() != PieceType.none && GameManager.tiles[testedPosition].GetChild().Piece.GetColour() 
+                            != int(isPlayerWhite) then
+                        break
+                    endif
+                else
+                    break
+                endif
+            previous7th i
+
+            for i = int(position.x * 8 + position.y + 7) to 63
+                if CheckLegality(isPlayerWhite, PieceType.bishop, isPlayerWhite, originPosition) then
+                    legalMoves.Add(GameManager.tiles[i])
+
+
+                    if GameManager.tiles[i].GetChild().Piece.GetType() != PieceType.none && GameManager.tiles[testedPosition].GetChild().Piece.GetColour() 
+                            != int(isPlayerWhite) then
+                        break
+                    endif
+                else
+                    break
+                endif
+            next7th i
+
+            for i = int(position.x * 8 + position.y - 9) to 0
+                if CheckLegality(isPlayerWhite, PieceType.bishop, isPlayerWhite, originPosition) then
+                    legalMoves.Add(GameManager.tiles[i])
+
+
+                    if GameManager.tiles[i].GetChild().Piece.GetType() != PieceType.none && GameManager.tiles[testedPosition].GetChild().Piece.GetColour() 
+                            != int(isPlayerWhite) then
+                        break
+                    endif
+                else
+                    break
+                endif
+            previous9th i
+        endif
+
+        # king
+        if pieceType == PieceType.king then
+            currentPosition = int(position.x * 8 + position.y)
+            if CheckLegality(currentPosition + 1, PieceType.king, isPlayerWhite White) && originPosition.y < 7 then
+                legalMoves.Add(GameManager.tiles[currentPosition + 1])
+            if CheckLegality(currentPosition - 1, PieceType.king, isPlayerWhite White) && originPosition.y > 0 then
+                legalMoves.Add(GameManager.tiles[currentPosition - 1])
+            if CheckLegality(currentPosition - 7, PieceType.king, isPlayerWhite White) && originPosition.y < 7 then
+                legalMoves.Add(GameManager.tiles[currentPosition - 7])
+            if CheckLegality(currentPosition + 9, PieceType.king, isPlayerWhite White) && originPosition.y < 7 then
+                legalMoves.Add(GameManager.tiles[currentPosition + 9])
+            if CheckLegality(currentPosition + 8, PieceType.king, isPlayerWhite White) then
+                legalMoves.Add(GameManager.tiles[currentPosition + 8])
+            if CheckLegality(currentPosition - 8, PieceType.king, isPlayerWhite White) then
+                legalMoves.Add(GameManager.tiles[currentPosition - 8])
+            if CheckLegality(currentPosition - 9, PieceType.king, isPlayerWhite White) && originPosition > 0 then
+                legalMoves.Add(GameManager.tiles[currentPosition - 9])
+            if CheckLegality(currentPosition + 7, PieceType.king, isPlayerWhite White) && originPosition > 0 then
+                legalMoves.Add(GameManager.tiles[currentPosition + 7])
+        endif
+        
+        return legalMoves
     endfunction
+
+    # Seperate function to check if moves are legal
+    private function CheckLegality(tileIndex, PieceType type, isPlayerWhite, originPosition = 0)
+        if tileIndex >= 0 && tileIndex <= 63 then
+            piece = GameManager.tiles[tileIndex].GetChild().Piece
+            tile = GameManager.tiles[tileIndex].Tile
+
+            switich type:
+                case PieceType.knight:
+                    return piece.GetType() == PieceType.none or (piece.GetType() != PieceType.none and piece.GetColour() != int (isPlayerWhite)
+                case PieceType.pawn:
+                    return piece.GetType() != PieceType.none and piece.GetColour() != int(isPlayerWhite)
+                case PieceType.rook:
+                    return piece.GetType() == PieceType.None or (piece.GetType() != PieceType.none and piece.GetColour() != int(isPlayerWhite))
+                case PieceType.bishop:
+                    return ((piece.GetType() == PieceType.none) or piece.GetType() != PieceType.none and piece.GetColour() != int(isPlayerWhite))   
+                            and tile.GetColour() == GameManager.tiles[originPosition].Tile.GetColour()
+                case PieceType.king:
+                    return piece.GetType() == PieceType.none or (piece.GetType() != PieceType.none and piece.GetColour() != int(isPlayerWhite))
+            endswitch
+        else
+            return false
+        endif
 endclass
